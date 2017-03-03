3a08c5e2787bd4683e556346f3ab050b328ade03
William Martins <wwwhmartins@gmail.com>
2017-03-03 18:17:21 -0300

    Move storybook to .storybook folder

--- .storybook/stories/InfiniteScroll.js
+++ .storybook/stories/InfiniteScroll.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import InfiniteScroll from '../../../src';
+import InfiniteScroll from '../../src';
 
 storiesOf(' > InfiniteScroll', module)
     .add('test', () => (
